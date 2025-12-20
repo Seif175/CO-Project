0d932aad4ee397d6145de90b9a63826d60413ed8
Seif Emad <Seifemad@hotmail.com>
2025-12-20 18:59:11 +0200

    test

---
+++ CO Project.asm
@@ -0,0 +1,13 @@
+.data
+msg: .asciiz "Hello World\n"
+
+.text
+.globl main
+
+main:
+    li $v0, 4        # syscall: print string
+    la $a0, msg     # load address of msg
+    syscall
+
+    li $v0, 10       # syscall: exit
+    syscall
