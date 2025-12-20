14c9eb9ed1c5e4645a0326db4a4a553adec558a2
Seif Emad <Seifemad@hotmail.com>
2025-12-20 21:47:11 +0200

    finish the inputs and outputs of user

--- CO Project.asm
+++ CO Project.asm
@@ -1,13 +1,76 @@
 .data
-msg: .asciiz "ttttt\n"
+msg: .asciiz "Do you want to perform encryption/decryption using Rail Fence Cipher? (Yes or No): "
+msg2: .asciiz "Enter plaintext message: "
+msg3: .asciiz "Enter numeric key (number of rails ≥ 2): "
+msg4: .asciiz "Generated Ciphertext: "
+msg5: .asciiz "Do you want to decrypt the ciphertext? (Y/N): "
+msg6: .asciiz "Decrypted Plaintext: "
+msg7: .asciiz "Yes\n"
+msg8: .asciiz "No\n"
+buffer: .space 100
 
 .text
 .globl main
 
 main:
-    li $v0, 4        # syscall: print string
-    la $a0, msg     # load address of msg
+    # Print The Prompt message
+    li $v0, 4
+    la $a0, msg
     syscall
 
-    li $v0, 10       # syscall: exit
+    # Read a string from the user
+    li $v0, 8
+    la $a0, buffer
+    li $a1, 100
     syscall
+
+    la $t0, msg7
+    la $t1, buffer
+
+    compare:
+        lb $t2, 0($t0)  
+        lb $t3, 0($t1)   
+
+        bne $t2, $t3, not_equal  
+
+        beq $t2, $zero, equal 
+
+        addi $t0, $t0, 1
+        addi $t1, $t1, 1
+        j compare
+
+    equal:
+        # Print The Prompt message
+        li $v0, 4
+        la $a0, msg2
+        syscall
+
+        # Read a string from the user
+        li $v0, 8
+        la $a0, buffer
+        li $a1, 100
+        syscall
+
+        # Print The Prompt message
+        li $v0, 4
+        la $a0, msg3
+        syscall
+
+        # Read an integer from the user
+        li $v0, 5
+        syscall
+        move $t0, $v0
+
+        # Print The Prompt message
+        li $v0, 4
+        la $a0, msg4
+        syscall
+
+
+
+        li $v0, 10       # syscall: exit
+        syscall
+
+    not_equal:
+        li $v0, 10
+        syscall
