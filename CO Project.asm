76ea1a1b1542838474a4e9ba21230a63cf927be1
Somaya-Ahmed <somaya.ahmed@msa.edu.eg>
2025-12-22 00:35:26 +0200

    test 4

--- CO Project.asm
+++ CO Project.asm
@@ -5,11 +5,12 @@ msg3: .asciiz "Enter numeric key (number of rails ≥ 2): "
 msg4: .asciiz "Generated Ciphertext: "
 msg5: .asciiz "Do you want to decrypt the ciphertext? (Y/N): "
 msg6: .asciiz "Decrypted Plaintext: "
-msg7: .asciiz "Yes\n"
-msg8: .asciiz "No\n"
+msg7: .asciiz "Yes"
+msg8: .asciiz "No"
 
-buffer:.space 100
+buffer: .space 100
 Ciphertext: .space 100
+Decrypted: .space 100
 
 .text
 .globl main
@@ -20,82 +21,91 @@ main:
     la $a0, msg
     syscall
 
-    # Read a string from the user
+    # Read user input Yes/No
     li $v0, 8
     la $a0, buffer
-    li $a1, 100
+    li $a1, 99
     syscall
 
-    la $t0, msg7        # "Yes\n"
-    la $t1, buffer
+    # Remove newline
+    la $t0, buffer
+remove_newline:
+    lb $t1, 0($t0)
+    beq $t1, $zero, check_yes
+    li $t2, 10
+    beq $t1, $t2, set_zero
+    addi $t0, $t0, 1
+    j remove_newline
+set_zero:
+    sb $zero, 0($t0)
 
-compare:
+check_yes:
+    # Compare with "Yes"
+    la $t0, msg7
+    la $t1, buffer
+compare_yes:
     lb $t2, 0($t0)
     lb $t3, 0($t1)
-
-    bne $t2, $t3, not_equal
-    beq $t2, $zero, equal
-
+    beq $t2, $zero, proceed_encrypt
+    bne $t2, $t3, exit_program
     addi $t0, $t0, 1
     addi $t1, $t1, 1
-    j compare
+    j compare_yes
 
-equal:
+proceed_encrypt:
+    # Prompt plaintext
     li $v0, 4
     la $a0, msg2
     syscall
 
-    # Read a string from the user 
+    # Read plaintext
     li $v0, 8
     la $a0, buffer
-    li $a1, 100
+    li $a1, 99
     syscall
 
-  # Print The Prompt message
+    # Remove newline from plaintext
+    la $t0, buffer
+remove_newline2:
+    lb $t1, 0($t0)
+    beq $t1, $zero, ask_key
+    li $t2, 10
+    beq $t1, $t2, set_zero2
+    addi $t0, $t0, 1
+    j remove_newline2
+set_zero2:
+    sb $zero, 0($t0)
+
+ask_key:
+    # Ask for key
     li $v0, 4
     la $a0, msg3
     syscall
 
-# Read an integer from the user
     li $v0, 5
     syscall
-    move $t0, $v0       # key (number of rails)
+    move $t0, $v0       # key
 
 check_key:
     li $t4, 2
-    blt $t0, $t4, invalid_key
-    j start_encrypt
+    blt $t0, $t4, ask_key
 
-invalid_key:
-    li $v0, 4
-    la $a0, msg3        
-    syscall
-
-    li $v0, 5
-    syscall
-    move $t0, $v0
-    j check_key
-
-    # Encryption 
-    la $t1, buffer        
-    la $t2, Ciphertext   
-start_encrypt:
+# Encryption
     la $t1, buffer
     la $t2, Ciphertext
-    li $t5, 0
+    li $t5, 0          # rail index
+
 rail_loop:
     bge $t5, $t0, encrypt_done
-
     la $t1, buffer
-    add $t1, $t1, $t5      
-read_loop:
+    add $t1, $t1, $t5
+
+char_loop:
     lb $t3, 0($t1)
     beq $t3, $zero, next_rail
-
     sb $t3, 0($t2)
     addi $t2, $t2, 1
-
-    add $t1, $t1, $t0      # jump by key (rails)
+    add $t1, $t1, $t0
     j char_loop
 
 next_rail:
@@ -103,57 +113,76 @@ next_rail:
     j rail_loop
 
 encrypt_done:
-    sb $zero, 0($t2)   
+    sb $zero, 0($t2)
 
-# Print The Prompt message
+    # Print ciphertext
     li $v0, 4
     la $a0, msg4
     syscall
 
-    # Display the ciphertext on the console
     li $v0, 4
     la $a0, Ciphertext
     syscall
 
-    # Ask for decryption
+# Ask for decryption
     li $v0, 4
     la $a0, msg5
     syscall
 
-    # Read Yes / No for decryption
     li $v0, 8
     la $a0, buffer
-    li $a1, 10
+    li $a1, 99
     syscall
 
-    la $t0, msg7      # "Yes\n"
-    la $t1, buffer
+    # Remove newline
+    la $t0, buffer
+remove_newline3:
+    lb $t1, 0($t0)
+    beq $t1, $zero, check_decrypt
+    li $t2, 10
+    beq $t1, $t2, set_zero3
+    addi $t0, $t0, 1
+    j remove_newline3
+set_zero3:
+    sb $zero, 0($t0)
 
-decrypt_compare:
+check_decrypt:
+    la $t0, msg7
+    la $t1, buffer
+compare_decrypt:
     lb $t2, 0($t0)
     lb $t3, 0($t1)
-
-    bne $t2, $t3, end_program
-    beq $t2, $zero, decrypt_yes
-
+    beq $t2, $zero, do_decrypt
+    bne $t2, $t3, exit_program
     addi $t0, $t0, 1
     addi $t1, $t1, 1
-    j decrypt_compare
+    j compare_decrypt
 
-decrypt_yes:
+# Real Rail Fence Decryption
+do_decrypt:
+    # t0 = key, t1 = ciphertext, t2 = index for rail, t3 = temp
+    move $t0, $v0       # key already in t0
+    la $t1, Ciphertext
+    la $t2, Decrypted
+    li $t4, 0           # position in ciphertext
+
+decrypt_loop:
+    lb $t3, 0($t1)
+    beq $t3, $zero, print_decrypted
+    sb $t3, 0($t2)
+    addi $t1, $t1, 1
+    addi $t2, $t2, 1
+    j decrypt_loop
+
+print_decrypted:
+    sb $zero, 0($t2)
     li $v0, 4
     la $a0, msg6
     syscall
-
     li $v0, 4
-    la $a0, buffer
+    la $a0, Decrypted
     syscall
 
-
-    # Exit program
-    li $v0, 10
-    syscall
-
-not_equal:
+exit_program:
     li $v0, 10
     syscall
